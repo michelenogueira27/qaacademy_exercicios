e506e5576decf0052ab1ae05f7ee01168ad7863b
Nogueira <michele.nogueira@capgemini.com>
2022-04-02 13:38:01 -0300

    Exercicio04-easy

--- src/easy/Exercicio04.java
+++ src/easy/Exercicio04.java
@@ -3,11 +3,10 @@ package easy;
 import javax.swing.JOptionPane;
 
 public class Exercicio04 {
-    public static void main(String[] args) {
-        int numero;
-        String numeroDigitado = JOptionPane.showInputDialog("Digite um numero");
-        numero = Integer.parseInt( numeroDigitado);
-        System.out.println("O Resultado:" + numero * 2);
+    public int calcularDobro(int numero) {
+        int resultado = numero * 2;
+        return resultado;
+    }
     }
 
-}
+
--- src/easy/execucao.java
+++ src/easy/execucao.java
@@ -5,8 +5,11 @@ public class execucao {
         // Exercicio02 exercicio02 = new Exercicio02();
         // System.out.println(Exercicio02.retornaPalavraDigitada("teste palavra"));
         
-        Exercicio03 exercicio03 = new Exercicio03();
-        System.out.println(exercicio03.exibeValoresTrocados(10, 20));
+        // Exercicio03 exercicio03 = new Exercicio03();
+        // System.out.println(exercicio03.exibeValoresTrocados(10, 20));
+
+        Exercicio04 exercicio04 = new Exercicio04();
+        System.out.println("O dobro do numero enviado: "+ 10 +" Será: "+exercicio04.calcularDobro(10));
     }
 
 }
