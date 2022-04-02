493edbca1063ce53ab9fb846e877c0bdf7a5f29c
Nogueira <michele.nogueira@capgemini.com>
2022-04-02 14:02:46 -0300

    Exercicio05- easy

--- src/easy/Exercicio05.java
+++ src/easy/Exercicio05.java
@@ -3,35 +3,23 @@ package easy;
 import javax.swing.JOptionPane;
 
 public class Exercicio05 {
-    public static void main(String[] args) {
-        double numero1,numero2, numero3;
-        double soma,subtracao,multiplicacao,media;
 
-        String numero1Digitado = JOptionPane.showInputDialog("Digite o primeiro numero");
-        String numero2Digitado = JOptionPane.showInputDialog("Digite o segundo numero");
-        String numero3Digitado = JOptionPane.showInputDialog("Digite o terceiro numero");
+    public int calcularSoma(int numero1, int numero2, int numero3) {
+        int soma = numero1 + numero2 + numero3;
+        return soma;
+    }
 
-        numero1= Double.parseDouble(numero1Digitado);
-        numero2= Double.parseDouble(numero2Digitado);
-        numero3= Double.parseDouble(numero3Digitado);
+    public int calcularSubtracao(int numero1, int numero2, int numero3) {
+        return numero1 - numero2 - numero3;
+    }
 
-        soma= numero1 + numero2 + numero3;
-        subtracao = numero1 - numero2 - numero3;
-        multiplicacao = numero1 * numero2 * numero3;
-        media = (soma)/3;
+    public int calcularMultiplicacao(int numero1, int numero2, int numero3) {
+        return numero1 * numero2 * numero3;
+    }
 
-        System.out.println("A soma dos numeros digitados é:" + soma);
-        System.out.println("A subtração dos numeros digitados é:" + subtracao);
-        System.out.println("A multiplicação dos numeros digitados é:" + multiplicacao);
-        System.out.println("A média dos numeros digitados é:" + media);
-
-
-
-        
-        
-
-
+    public double calcularMedia(double numero1, double numero2, double numero3) {
+        return (numero1 + numero2 + numero3) / 3;
 
     }
-    
+
 }
